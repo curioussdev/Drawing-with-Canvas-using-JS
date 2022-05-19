01dc95d3c216606e8df21d61d4743f059fa421fe
Jose Lopes <946393361bill@gmail.com>
2022-05-19 22:48:12 +0100

    criando funções das condições de desenho

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+/**
+ * PASSO A PASSO PARA DESENHAR NO CANVAS 
+ * - Quando o mouse CLICAR/BAIXAR, ative o modo desenho
+ * - Quando o mouse se MOVER, se o modo desenho estiver ativado, desenhe
+ * Só desenha quando estas duas codnições estiverem satisfeitas
+ * - Quando o click do mouse LEVANTAR, desative o modo desenho
+ */
+
+
+
 // INICIAL DATA
 let currentColor = '#000000';
 
@@ -10,22 +21,28 @@ document.querySelectorAll('.colorArea .color').forEach( item =>{
     item.addEventListener('click', colorClickEvent);
 });
 
-/**
- * PASSO A PASSO PARA DESENHAR NO CANVAS 
- * - Quando o mouse CLICAR/BAIXAR, ative o modo desenho
- * - Quando o mouse se MOVER, se o modo desenho estiver ativado, desenhe
- * Só desenha quando estas duas codnições estiverem satisfeitas
- */
-
-
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
 
 //FUNCTIONS
 function colorClickEvent(e) {
     let color = e.target.getAttribute('data-color'); // BUSCANDO COR CLICADA
     currentColor = color;
 
-
-
     document.querySelector('.color.active').classList.remove('active'); // Remover borda de "active" da cor que conter
     e.target.classList.add('active'); // adicionar a borda de "active na cor clicada"
-}
+};
+
+function mouseDownEvent() {
+
+};
+
+function mouseMoveEvent() {
+
+};
+
+function mouseUpEvent() {
+
+};
+
