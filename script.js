76a357803c4815df298667a0e510935ba22edf6e
Jose Lopes <946393361bill@gmail.com>
2022-05-19 22:12:33 +0100

    removendo e add borda 'active' das cores clicadas

--- script.js
+++ script.js
@@ -7,4 +7,9 @@ document.querySelectorAll('.colorArea .color').forEach( item =>{
 function colorClickEvent(e) {
     let color = e.target.getAttribute('data-color'); // BUSCANDO COR CLICADA
     currentColor = color;
+
+
+
+    document.querySelector('.color.active').classList.remove('active'); // Remover borda de "active" da cor que conter
+    e.target.classList.add('active'); // adicionar a borda de "active na cor clicada"
 }
