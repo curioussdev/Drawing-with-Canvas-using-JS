a7823116c799a904edb6dc7cd0e8537ba77e69e9
Jose Lopes <946393361bill@gmail.com>
2022-05-19 22:06:29 +0100

    Detectar click na COR

---
+++ script.js
@@ -0,0 +1,10 @@
+let currentColor = '#000000';
+
+document.querySelectorAll('.colorArea .color').forEach( item =>{
+    item.addEventListener('click', colorClickEvent);
+});
+
+function colorClickEvent(e) {
+    let color = e.target.getAttribute('data-color'); // BUSCANDO COR CLICADA
+    console.log(`COR ${color} CLICADA`)
+}
